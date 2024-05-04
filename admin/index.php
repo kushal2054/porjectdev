54f6c4ddd6122221e051e2733c685db0297743b6
kushal2054 <62107458+kushal2054@users.noreply.github.com>
2024-05-04 20:33:27 +0545

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,44 @@
+<?php include('includes/header.php'); ?>
+<h2>Welcome to admin Dashboard</h2>
+<div class="row">
+        <div class="col-md-3 mb-4">
+            <div class="card card-body p-3">
+                <p class="text-sm mb-0 text-capitalize font-weight-bold">Total Users</p>
+                <h5 class="font-weight-bolder mb-0">800</h5>
+            </div>
+        </div>
+
+        <div class="col-md-3 mb-4">
+            <div class="card card-body p-3">
+                <p class="text-sm mb-0 text-capitalize font-weight-bold">Active Users</p>
+                <h5 class="font-weight-bolder mb-0">600</h5>
+            </div>
+        </div>
+
+        <div class="col-md-3 mb-4">
+            <div class="card card-body p-3">
+                <p class="text-sm mb-0 text-capitalize font-weight-bold">Block Users</p>
+                <h5 class="font-weight-bolder mb-0">200</h5>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6" style="border-width: 5px; border-color:black;">
+</div>
+
+
+    <div class="row">
+        <div class="col md-12">
+            <div class="card">
+                <div class="card-header">
+                <h2>User Graph</h2>
+                <img src="assets/img/graph.png" >
+                </div>
+                <div class="card-body">
+
+
+                </div>
+            </div>
+        </div>
+   </div>
+
+<?php include('includes/footer.php'); ?>
